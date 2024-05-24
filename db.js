9309c35cffb8c051614448d3073a05c3d11fee19
devabdulsalam <ammuftau74@gmail.com>
2024-05-24 12:33:35 +0100

    add err

--- db.js
+++ db.js
@@ -9,7 +9,10 @@ const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 });
 pool.connect((err) => {
-	if (err) throw err;
+	if (err) {
+		console.log(err);
+		throw err;
+	}
 	console.log('Connect to PostgreSQL successfully!');
 });
 
